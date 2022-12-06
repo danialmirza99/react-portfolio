915af7cc5a96991c8746332f9011bbff0142ed17
danialmirza99 <danialmirza99@gmail.com>
2022-12-05 22:03:37 -0800

    Added about me

---
+++ portfolio/src/components/AboutMe.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/AboutMe.css';
+
+
+function AboutMe() {
+  return (
+    <section className="about-me">
+      <p>
+        My name is Danial Mirza. I am a recent graduate from UC Riverside. I live in California. I am currently working remotely for a research lab.
+      </p>
+    </section>
+  );
+}
+
+export default AboutMe;
--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -6,15 +6,20 @@ const styles = {
     background: 'red',
   },
   headingStyle: {
-    fontSize: '100px',
+    fontSize: '60px',
   },
 };
 
 function Header() {
   return (
-    <header style={styles.headerStyle} className="header">
-      <h1 style={styles.headingStyle}>
-        Danial
+    <header className="header">
+      <h1>
+        <ul>
+          <li>Danial</li>
+          <li><a href="#about-me" class="float-right">About Me</a></li>
+          <li><a href="#portfolio" class="float-right">Portfolio</a></li>
+          <li><a href="#resume" class="float-right">Contact</a></li>
+        </ul>
       </h1>
     </header>
   );
