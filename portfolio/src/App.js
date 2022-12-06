44ce28d8cbfb2966bcce2fb4ad83f6818af229df
danialmirza99 <danialmirza99@gmail.com>
2022-12-05 21:13:12 -0800

    Removed Navbar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,13 +2,11 @@ import React from "react";
 import './App.css';
 import Header from "./components/Header";
 import Section from "./components/Section";
-import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
-      <Navbar />
       <Header />
       <Section />
       <Footer />
